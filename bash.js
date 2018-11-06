ef55f17e780eb5b79ef39750602ed2922090c37c
Sabira Davletshina <sabira.davletshina@gmail.com>
2018-11-06 14:57:00 -0500

    completed till bonus

---
+++ bash.js
@@ -0,0 +1,10 @@
+process.stdout.write('prompt >');
+const pwd = require('./pwd');
+const ls = require('./ls')
+const cat = require('./cat');
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  if(cmd === 'pwd') pwd();
+  else if(cmd === 'ls') ls();
+  else if (cmd.split(' ')[0] === 'cat') cat(cmd.split(' ')[1])
+})
